93edf00494d5764b8c88a8acb77011a49fe77127
matiaih <233924@students.aih.nsw.edu.au>
2024-05-27 20:35:25 +1000

    Update session.py

--- session.py
+++ session.py
@@ -14,3 +14,28 @@ apple_gross = 75
 pie_gross = 100
 total_gst = (apple_gross/100*10) + (pie_gross/100*8)
 print(total_gst)
+
+# Write a program to calculate the number of minis in the maxis
+# Total maxis s 2.5*minis
+# 1 mini is 5000
+# Calculate how much is 4.5 maxis
+
+value for minis is 5000
+maxis is 2.5*minis
+so 4.5 maxis is equal to 2.5*4.5*minis
+display maxis
+
+minis = 5000
+maxis = 2.5*minis
+print("value of maxis is :",4.5*maxis)
+
+
+bits = 1
+byte = 1*8
+kilo_byte = 1024*byte
+mega_byte = 1024*kilo_byte
+giga_byte = 1024*mega_byte
+terra_byte = 1024*giga_byte
+
+print("The memory allocation units are as follows")
+print("1.", bits , "2.", byte , "3.", kila_byte , "4.", mega_byte, "5." , giga_byte , "6.", terra_byte)
